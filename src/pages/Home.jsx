7e1c09cee4a9d591ec2cbb6d462f2b50c5cc4e07
sumitpd8 <sumitprasad423@gmail.com>
2025-07-07 23:10:16 +0530

    init

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -281,7 +281,7 @@ const Home = () => {
         {/* Stadium Background Image */}
         <div 
           className="absolute inset-0 bg-cover bg-center bg-no-repeat opacity-100"
-          style={{ backgroundImage: `url(/images/Gallary/Background2.jpg)` }}>
+          style={{ backgroundImage: `url(/images/Gallary/Background3.jpg)` }}>
         </div>
         <div className="absolute inset-0 bg-black opacity-20"></div>
         <div className="relative max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 text-center">
